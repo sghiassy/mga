af89861fe146b7051f3337206077f7331c1eee8e
Shaheen Ghiassy <shaheen@groupon.com>
2017-09-30 22:15:30 -0700

    Initial Commit

---
+++ mga/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mga
+//
+//  Created by Shaheen Ghiassy on 9/30/17.
+//  Copyright © 2017 Shaheen Ghiassy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
